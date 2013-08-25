5478f144c7e183b66abd80c53b503753882bc00e
Brent Westmoreland <git@westmoreland.us>
2013-08-24 21:20:35 -0400

    Implement silver challenge

---
+++ Ch16/Homepwner - Silver/Homepwner/AssetTypePicker.m
@@ -0,0 +1,111 @@
+//
+//  AssetTypePicker.m
+//  Homepwner
+//
+//  Created by Brent Westmoreland on 8/24/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+#import "AssetTypePicker.h"
+#import "Item.h"
+#import "ItemStore.h"
+#import "AddAssetTypeViewController.h"
+
+@interface AssetTypePicker()
+
+@property (nonatomic, strong) Class cellClass;
+
+@end
+
+@implementation AssetTypePicker
+
+- (id)init
+{
+    return [super initWithStyle: UITableViewStyleGrouped];
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+- (Class)cellClass
+{
+    return [UITableViewCell class];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass: self.cellClass
+           forCellReuseIdentifier: NSStringFromClass(self.cellClass)];
+    
+    UIBarButtonItem *addButtonItem;
+    addButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem: UIBarButtonSystemItemAdd
+                                                                  target: self
+                                                                  action: @selector(addAssetType:)];
+    self.navigationItem.rightBarButtonItem = addButtonItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear: YES];
+    [self.tableView reloadData];
+}
+
+- (void)addAssetType: (id)sender
+{
+    AddAssetTypeViewController *addAssetTypeViewController = [[AddAssetTypeViewController alloc] initWithNibName: nil
+                                                                                                          bundle: nil];
+    
+    [self.navigationController pushViewController: addAssetTypeViewController
+                                         animated: YES];
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[ItemStore sharedStore] allAssetTypes] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: NSStringFromClass(self.cellClass)
+                                                forIndexPath: indexPath];
+    
+    NSArray *allAssets = [[ItemStore sharedStore] allAssetTypes];
+    NSManagedObject *assetType = allAssets[indexPath.row];
+    
+    NSString *assetLabel = [assetType valueForKey: @"label"];
+    cell.textLabel.text = assetLabel;
+    
+    if (assetType == self.item.assetType) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath: indexPath];
+    
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    NSArray *allAssets = [[ItemStore sharedStore] allAssetTypes];
+    NSManagedObject *assetType = allAssets[indexPath.row];
+    self.item.assetType = assetType;
+    
+    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+        [self.delegate didSelectItem: self];
+    }
+    else {
+        [self.navigationController popViewControllerAnimated: YES];
+    }
+
+}
+
+@end
